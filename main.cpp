32542a7eed0cac2c13f6a83ddea741f8fac65b6c
Huggins <1339903191@qq.com>
2024-04-11 23:56:40 +0800

    嵌入式开发学习路线

--- main.cpp
+++ main.cpp
@@ -6,3 +6,23 @@ int main()
     cout << "hello C++!" << endl; 
     return 0;
 }
+
+/*struct ListNode* removeDuplicateNodes(struct ListNode* head){
+    // 判断是否需要遍历
+    if(head == NULL) return NULL;
+    struct ListNode* current = head;
+    // 双指针逐个比较
+    while (current)
+    {
+        struct ListNode* p = current;
+        while(p->next){
+            if(p->next->val == current->cal){
+                p->next = p->next->next;
+            }else{
+                p = p->next;
+            }
+        }
+        current = current->next;
+    }
+    return head;
+}*/
